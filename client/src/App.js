e05c6e2c20c50f86871e7ee80b75707868aa19fc
emmab74330 <emmab74330@gmail.com>
2022-06-08 11:53:07 +0200

    MAJ
    Mise à jour du code.

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import Ville from './containers/Ville';
 import ModifListe from './containers/Modif_Liste';
 
 export default function App() {
-  const [listeVilles, setListeVilles] = useState([]);
-  const [ville, setville] = useState([])
+  const [ListeVilles, setListeVilles] = useState([]);
+  const [Temps, setTemps] = useState([]);
+  const [Degres, setDegres] = useState([]);
 
   useEffect(() => {
     // faire une requete avec axios.get
@@ -15,15 +16,35 @@ export default function App() {
       "Paris",
       "Bordeaux"
     ])
+    setTemps([
+      "Nuageux",
+      "Ensoleillé",
+      "Pluie"
+    ])
+    setDegres([
+      "12°C",
+      "16°C",
+      "27°C"
+    ])
   }, [])
   return (
     <div>
       {
-        listeVilles.map((ville) => {
+        ListeVilles.map((ville) => {
           return <Ville city={ville}></Ville>
         })
       }
-      < ModifListe listeVilles={listeVilles}></ModifListe>
+      {
+        Temps.map((Temps) => {
+          return <Ville weather={Temps}></Ville>
+        })
+      }
+      {
+        Degres.map((Degres) => {
+          return <Ville heat={Degres}></Ville>
+        })
+      }
+      < ModifListe ListeVilles={ListeVilles}></ModifListe>
     </div >
   )
 }
--- client/src/containers/Ville.js
+++ client/src/containers/Ville.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 function Ville(props) {
     return <border>
         <center>{props.city}</center>
-        <center>{props.temps}</center>
-        <center>{props.temperature}</center>
+        <center>{props.weather}</center>
+        <center>{props.heat}</center>
     </border>
 }
 export default Ville;
