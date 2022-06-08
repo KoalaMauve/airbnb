2f5e36b7f7084df3c1a164902db77b0d975381b4
KoalaMauve <gab74segh@gmail.com>
2022-06-08 11:20:13 +0200

    add transformKelvin function in utils

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,8 +1,18 @@
-function kelvin(kelvin) {
+function kelvinToCelcius(kelvin) {
     celcius = parseInt((kelvin - 273.15).toFixed(0), 10)
     return celcius
 }
 
+function transformKelvin(object, keys) {
+    object.data.list.forEach(element => {
+        keys.forEach(key => {
+            element.main[key] = kelvinToCelcius(element.main[key])
+        });
+    });
+
+}
+
 module.exports = {
-    kelvin
+    kelvinToCelcius,
+    transformKelvin
 };
