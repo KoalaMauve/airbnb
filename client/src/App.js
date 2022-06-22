2b62f6616e016c98b94a5a40eb0dff171c9c5660
emmab74330 <emmab74330@gmail.com>
2022-06-22 09:31:31 +0200

    MAJ code
    requete axios.get
    ajout commentaire

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,43 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Ville from './containers/Ville';
 import ModifListe from './containers/Modif_Liste';
+import axios from 'axios';
 
 export default function App() {
+  //Définition des variables
   const [ListeVilles, setListeVilles] = useState([]);
   const [Temps, setTemps] = useState([]);
   const [Degres, setDegres] = useState([]);
 
   useEffect(() => {
-    // faire une requete avec axios.get
-    // sur le point d'API de Gabriel
+    //async function + requête axios.get sur API pour avoir la météo
+    async function getWeather() {
+      var response = await axios.get('https://')
+      setTemps(response.data)
+    }
+
+    //MAJ liste villes
     setListeVilles([
       "Annecy",
       "Paris",
       "Bordeaux"
     ])
+
+    //MAJ temps
     setTemps([
       "Nuageux",
       "Ensoleillé",
       "Pluie"
     ])
+
+    //MAJ Températures
     setDegres([
       "12°C",
       "16°C",
       "27°C"
     ])
   }, [])
-  return (
+  return ( //
     <div>
       {
         ListeVilles.map((ville) => {
